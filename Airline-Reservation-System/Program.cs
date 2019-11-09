ec0746ab50402691b86da01b4a918f4c0f935d85
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2019-11-09 01:13:04 -0700

    finsh adding extra variables

--- Airline-Reservation-System/Program.cs
+++ Airline-Reservation-System/Program.cs
@@ -16,21 +16,52 @@ namespace Airline_Reservation_System
 
             for (int i = 0; i < 5; i++)
             {
-                Console.Write(seats[i] + " ");
+                Console.Write(seats[i] ? "Booked " : "Empty ");
             }
 
             Console.WriteLine("\nEconomy Seats: ");
 
             for (int i = 5; i < 10; i++)
             {
-                Console.Write(seats[i] + " ");
+                Console.Write(seats[i] ? "Booked " : "Empty ");
             }
+        }
+
+        static bool checkFirstClass(bool[] seats)
+        {
+            bool firstClassFull = true;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (seats[i] == false)
+                {
+                    firstClassFull = false;
+                    break;
+                }
+            }
+
+            return firstClassFull;
+        }
+
+        static bool checkEconomy(bool[] seats)
+        {
+            bool economyFull = true;
+
+            for (int i = 5; i < 10; i++)
+            {
+                if (seats[i] == false)
+                {
+                    economyFull = false;
+                    break;
+                }
+            }
+
+            return economyFull;
         }
 
         static void Main(string[] args)
         {
-            bool isNum;
-            string userInput;
+            bool isNum, firstClassFull, economyFull;
             bool[] seats = new bool[10];
             int selectedSeat = 0;
 
@@ -41,8 +72,10 @@ namespace Airline_Reservation_System
 
             do
             {
-
                 printSeats(seats);
+
+                firstClassFull = checkFirstClass(seats);
+                economyFull = checkEconomy(seats);
 
                 isNum = false;
                 while (!isNum || selectedSeat > 10 || selectedSeat < 1)
@@ -59,7 +92,33 @@ namespace Airline_Reservation_System
 
                 if (selectedSeat == -1) { break; }
 
-                seats[selectedSeat - 1] = true;
+                if (firstClassFull && economyFull)
+                {
+                    Console.WriteLine("All seats are booked. The next flight leaves in 3 hours.");
+                } else
+                {
+                    if (!seats[selectedSeat - 1])
+                    {
+                        seats[selectedSeat - 1] = true;
+                        Console.WriteLine(selectedSeat <= 5 ? "Seat " + selectedSeat + " has been booked in First Class." : "Seat " + selectedSeat + " has been booked in Economy.");
+                    }
+                    else
+                    {
+                        if (firstClassFull && !economyFull)
+                        {
+                            Console.WriteLine("The First Class section is full. Please select a seat in the Economy section.");
+                        } else if (economyFull && !firstClassFull)
+                        {
+                            Console.WriteLine("The Economy section is full. Please select a seat in the First Class section.");
+                        } else
+                        {
+                            Console.WriteLine("That seat has already been booked. Please select a different seat.");
+                        }
+                    }
+                }
+
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
 
                 Console.Clear();
 
